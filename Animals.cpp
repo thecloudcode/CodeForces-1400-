49f4844dec74e04798b0541b70dde01227d78c2e
Badal Prasad Singh <badalstar1806@gmail.com>
2024-06-25 23:19:54 +0530

    DP-1700

---
+++ Animals.cpp
@@ -0,0 +1,33 @@
+/* Badal Prasad Singh */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fast  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define yes cout<<"YES"<<endl;
+#define lol long long int
+#define no cout<<"NO"<<endl;
+#define noo cout<<-1<<endl;
+#define anz cout<<ans<<endl;
+
+lol f(lol i, lol m, vector<lol>&x, vector<lol>&dp) {
+    if(i>=x.size() or m<0) return 0; lol pick = 0;
+    if(dp[i]!=-1) return dp[i];
+    if(m-(x.size()-i)*x[i]>=0) pick = 1 + f(i+1,m-(x.size()-i)*x[i],x,dp);
+    lol notpick = f(i+1,m,x,dp);
+    return max(pick,notpick);
+}
+
+void solve() {
+    lol n, m; cin>>n>>m;
+    vector<lol>x(n); for(lol i=0;i<n;i++) cin>>x[i];
+    vector<lol>dp(n+1,0);
+    cout<<f(0,m,x,dp)<<endl;
+}
+
+int main() {
+    fast;
+    lol t = 1;
+    // cin >> t;
+    while (t--) solve();
+}
---
+++ Mysterious Present.cpp
@@ -0,0 +1,29 @@
+// /* Badal Prasad Singh */
+//
+// #include<bits/stdc++.h>
+// using namespace std;
+//
+// #define fast  ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+// #define yes cout<<"YES"<<endl;
+// #define lol long long int
+// #define no cout<<"NO"<<endl;
+// #define noo cout<<-1<<endl;
+// #define anz cout<<ans<<endl;
+//
+// void solve() {
+//     lol n; lol a, b; cin>>n>>a>>b;
+//     vector<vector<lol>>x;
+//     for(lol i=0;i<n;i++) {
+//         vector<lol>xx(3); cin>>xx[0]>>xx[1]; xx[2]=i;
+//         x.push_back(xx);
+//     }
+//     sort(x.begin(), x.end());
+//
+// }
+//
+// int main() {
+//     fast;
+//     lol t = 1;
+//     // cin >> t;
+//     while (t--) solve();
+// }
